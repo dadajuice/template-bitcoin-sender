baafe5b70a7fff208872f284a49e90ba1cc9eba5
David Tucker <davidt2003@msn.com>
2022-10-06 21:52:04 -0400

    feat: add base index page with dependencies

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 router.get('/', function(req, res) {
   let publicAddress = "mj4CNS8gScsNDhZDqFCGJfghEMHRpvfg9t";
-  res.render('bitcoin-send', {
+  res.render('index', {
     balance: "0.00000",
     error: req.query.error_msg,
     success: req.query.success_msg,
