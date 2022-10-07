f7931e4ea97f18285dc9b145badc39fede664715
David Tucker <davidt2003@msn.com>
2022-10-06 21:45:33 -0400

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let router = express.Router();
+
+router.get('/', function(req, res) {
+  let publicAddress = "mj4CNS8gScsNDhZDqFCGJfghEMHRpvfg9t";
+  res.render('bitcoin-send', {
+    balance: "0.00000",
+    error: req.query.error_msg,
+    success: req.query.success_msg,
+    address: publicAddress
+  });
+});
+
+module.exports = router;
